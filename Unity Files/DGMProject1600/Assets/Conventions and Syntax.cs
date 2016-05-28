cbb9decf1f98a3826f2f6f85ea90cfdb3f7619d7
Dustin Porter <bigdporter@gmail.com>
2016-05-28 00:01:59 -0600

    Created Conventions and Syntax Script

---
+++ Unity Files/DGMProject1600/Assets/Conventions and Syntax.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicSyntax : MonoBehaviour
+{
+	void Start ()
+	{
+		Debug.Log(transform.position.x);
+
+		if(transform.position.y <= 5f)
+		{
+			Debug.Log ("This is my proof of knowing conventions and syntax in coding!");
+
+			Debug.Log ("My kids were sick this week so I couldn't make it to class but" +
+			"I definitely know conventions and syntax beacuse I have previously taken a C# and C++" +
+			"class at BYU and UVU!");
+		}
+	}
+}
