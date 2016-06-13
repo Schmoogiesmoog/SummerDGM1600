60ab34372fa2d7cd5cd9c5902c4b85c3b4a0abaf
Dustin Porter <bigdporter@gmail.com>
2016-06-13 11:22:46 -0600

    Ammo and Weapons and Loops Practice

---
+++ Unity Files/DGMProject1600/Assets/BaseAmmo.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+
+public class BaseAmmo {
+
+	//Controls the amount and power level of the ammo
+	public int ammoCount;
+	public int ammoPower;
+
+	// This function runs when called for a weapon
+	public void Fire()
+	{
+		if (ammoCount > 0) {
+			ammoCount--;
+			Debug.Log (ammoCount);
+		} else {
+			Debug.Log ("Out of ammo!");
+		}
+
+	}
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Unity Files/DGMProject1600/Assets/BaseWeapon.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaseWeapon : MonoBehaviour {
+
+	public BaseAmmo thisAmmo;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		thisAmmo = new BaseAmmo ();
+		thisAmmo.ammoCount = 10000;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		thisAmmo.Fire ();
+	
+	}
+}
