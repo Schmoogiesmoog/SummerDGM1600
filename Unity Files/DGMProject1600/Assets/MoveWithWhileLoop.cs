aef158a0011f4ca104ad0f06d1d631f525daccb9
Dustin Porter <bigdporter@gmail.com>
2016-06-15 11:20:03 -0600

    Learning while loops

---
+++ Unity Files/DGMProject1600/Assets/MoveWithWhileLoop.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveWithWhileLoop : MonoBehaviour {
+
+	public float xDistance = 10;
+	Vector3 pos;
+
+	// Update is called once per frame
+	void Update () {
+
+		while (pos.x <= xDistance) {
+			pos.x += 0.1f;
+		}
+		transform.position = pos;
+		print (pos.x);
+	}
+}
